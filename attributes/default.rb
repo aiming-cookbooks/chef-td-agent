00a11fba243881ff9d307c862c3c362e5ceb370e
futoase <futoase@gmail.com>
2013-11-11 16:53:02 +0900

    Modified: Added attribute of log directory.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,13 @@ node.default[:td_agent][:api_key] = ''
 node.default[:td_agent][:plugins] = []
 
 # TD environment settings
-node.default[:td_agent][:user]      = 'td-agent' 
-node.default[:td_agent][:home]      = '/var/run/td-agent'
-node.default[:td_agent][:owner]     = 'td-agent'
-node.default[:td_agent][:group]     = 'td-agent'
-node.default[:td_agent][:directory] = '/etc/td-agent/'
-node.default[:td_agent][:config]    = '/etc/td-agent/td-agent.conf'
+node.default[:td_agent][:user]          = 'td-agent' 
+node.default[:td_agent][:home]          = '/var/run/td-agent'
+node.default[:td_agent][:owner]         = 'td-agent'
+node.default[:td_agent][:group]         = 'td-agent'
+node.default[:td_agent][:directory]     = '/etc/td-agent/'
+node.default[:td_agent][:config]        = '/etc/td-agent/td-agent.conf'
+node.default[:td_agent][:log_directory] = '/var/log/td-agent'
 
 # TD package repository
 node.default[:td_agent][:package][:ubuntu] = 'http://packages.treasure-data.com/precise/'
