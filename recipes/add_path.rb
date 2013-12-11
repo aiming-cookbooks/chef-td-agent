304545e342cae0008d4bf1b079f06eaba31ac1be
futoase <futoase@gmail.com>
2013-12-11 11:57:09 +0900

    Modified: Added if statement of checking add_path.

--- recipes/add_path.rb
+++ recipes/add_path.rb
@@ -5,7 +5,9 @@
 # Copyright 2013, Aiming Inc.
 #
 
-template "/etc/profile.d/fluent_ruby_path.sh" do
-  source "fluent_ruby_path.sh.erb"
-  mode   0755
+if node.default[:td_agent][:ruby][:add_path] 
+  template "/etc/profile.d/fluent_ruby_path.sh" do
+    source "fluent_ruby_path.sh.erb"
+    mode   0755
+  end
 end
